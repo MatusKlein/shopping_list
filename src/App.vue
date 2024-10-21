ce18438b59d5789ddc72f67ae5557d4dbdd89880
MatusKlein <awesome.carrry@gmail.com>
2024-10-21 12:30:36 +0200

    shopping list completed

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,64 @@
 <template>
-  <input v-model="input" />
-  <button @click="addItem()">Add to list</button>
+  <h1>Nova polozka</h1>
+
+  <p style="margin-bottom: 0; font-size: 90%">Nazov polozky</p>
+  <input v-model="input" placeholder="Maslo" /><br />
+  <button @click="addItem">Pridať</button>
+
+  <p style="margin-top: 0">______________________________________</p>
+
+  <h2>Polozky</h2>
+  <ul>
+    <li v-for="item in validItems" :key="`item-${item.id}`">
+      <span @click="deleteItem(item)" style="margin-right: 15px">X</span>
+      {{ item.text }}
+    </li>
+  </ul>
+
+  <p style="margin-top: 0">______________________________________</p>
+
+  <h2>Zmazane polozky</h2>
+  <ul>
+    <li v-for="item in deletedItems" :key="`item-${item.id}`">
+      <span style="text-decoration: line-through; margin-right: 15px">
+        {{ item.text }}
+      </span>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      items: [],
       input: "",
-      list: [],
     };
   },
+
+  computed: {
+    validItems() {
+      return this.items.filter((item) => !item.is_deleted);
+    },
+
+    deletedItems() {
+      return this.items.filter((item) => item.is_deleted);
+    },
+  },
+
   methods: {
     addItem() {
-      this.list.push({
-        id: this.list.length + 1,
-        text: this.input,
-        is_deleted: false,
-      });
-      this.input = "";
+      if (this.input.trim()) {
+        this.items.push({
+          id: this.items.length + 1,
+          text: this.input,
+          is_deleted: false,
+        });
+        this.input = "";
+      }
+    },
+    deleteItem(item) {
+      item.is_deleted = true;
     },
   },
 };
@@ -29,7 +69,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
